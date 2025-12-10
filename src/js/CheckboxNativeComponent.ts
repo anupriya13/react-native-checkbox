03adb4c1bb7969e5366fda5163733342970353f3
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-12-10 16:59:11 +0530

    try fixing example

--- src/js/CheckboxNativeComponent.ts
+++ src/js/CheckboxNativeComponent.ts
@@ -5,6 +5,7 @@
 
 import type {ViewProps, ColorValue, HostComponent} from 'react-native';
 import type {BubblingEventHandler} from 'react-native/Libraries/Types/CodegenTypes';
+import {Platform} from 'react-native';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
 type CheckBoxEvent = Readonly<{
@@ -21,4 +22,20 @@ export interface NativeProps extends ViewProps {
   onChange?: BubblingEventHandler<CheckBoxEvent>;
 }
 
-export default codegenNativeComponent<NativeProps>('Checkbox') as HostComponent<NativeProps>;
+let CheckboxNativeComponent: HostComponent<NativeProps>;
+
+// Windows uses requireNativeComponent with static view config
+if (Platform.OS === 'windows') {
+  // @ts-ignore requireNativeComponent typing
+  const {requireNativeComponent} = require('react-native');
+  // Provide a static view config to avoid dynamic lookup in new architecture
+  CheckboxNativeComponent = requireNativeComponent('Checkbox', {
+    nativeOnly: {
+      onChange: true,
+    }
+  });
+} else {
+  CheckboxNativeComponent = codegenNativeComponent<NativeProps>('WindowsCheckBoxComponent') as HostComponent<NativeProps>;
+}
+
+export default CheckboxNativeComponent;
